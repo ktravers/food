fb2693d43b2d9e39c0d4833555271e0ff1df5606
ktravers <kate.travers@flatironschool.com>
2018-08-02 23:13:30 -0400

    create app

---
+++ lib/food/application.ex
@@ -0,0 +1,31 @@
+defmodule Food.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Start the Ecto repository
+      Food.Repo,
+      # Start the endpoint when the application starts
+      FoodWeb.Endpoint,
+      # Starts a worker by calling: Food.Worker.start_link(arg)
+      # {Food.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Food.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    FoodWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
